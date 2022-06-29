7d1042fffe993039e98ecfc3e2210bfc671e316d
miss-mad <mmlh.harris@gmail.com>
2022-06-29 16:03:00 -0400

    updates for tribute_page

---
+++ tribute_page.js
@@ -0,0 +1,15 @@
+//new js file for tribute page
+const greeting = 'this is me learning';
+alert(greeting);
+
+alert(2+3);
+
+let number1 = 2;
+let number2 = 3;
+let sum = number1 + number2;
+alert(sum);
+
+function add(number1, number2) {
+    let sum = number1 + number2;
+    return sum;
+}
